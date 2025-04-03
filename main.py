00712d2d61d42289ef9a4edd8fc36a0867d8ee8e
Jim Barlow <jim@jimbarlow.com>
2025-04-03 16:18:35 -0400

    redirect for membership

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return redirect("https://volunteers/harthumane.org/membership/")
+    # return render_template('home.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+  app.run()
