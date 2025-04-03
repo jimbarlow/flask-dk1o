4f7e76cfbabdd1270e3fe485cd86d825ef9b85b2
Jim Barlow <jim@jimbarlow.com>
2025-04-03 19:47:32 -0400

    move to zeffy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect("https://volunteers.harthumane.org/membership/")
+    return redirect("https://www.zeffy.com/ticketing/30404bdc-37d5-43a4-97b8-735c23c8de3a")
     # return render_template('home.html')
 
 if __name__ == "__main__":
